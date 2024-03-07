d8ce41b14b138dbf6c8563c41717b02eedfe6f4c
BallSpins <sandalupil@gmail.com>
2024-03-07 12:52:07 +0700

    yes.

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-window.onscroll = () => {scroll()};
+window.onscroll = () => {
+    (() => {
+        if(window.scrollY >= sticky) {
+            console.log("log")
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    })();
+};
 
 let navbar = document.getElementById("mobile-nav");
 let sticky = navbar.offsetTop;
+let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+/*let i = 0;
 
-function scroll() {
-    if(window.scrollY >= sticky) {
-        console.log("log")
-        navbar.classList.add("sticky");
+function hamburger() {
+    if(i == 0) {
+        navbar.classList.add("h-75");
+        i++;
     } else {
-        navbar.classList.remove("sticky");
+        navbar.classList.remove("h-75");
+        i--;
     }
-}
+}*/
+
+console.log(width);
