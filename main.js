412baf1b88c9b3d057bb4680670fbf526cb3268a
BallSpins <sandalupil@gmail.com>
2024-03-23 21:29:23 +0700

    y

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-window.onscroll = () => {
-    (() => {
-        if(window.scrollY >= sticky) {
-            console.log("log")
-            navbar.classList.add("sticky");
-        } else {
-            navbar.classList.remove("sticky");
-        }
-    })();
-};
+const express = require('express');
+const path = require('path');
+const app = express()
 
-let navbar = document.getElementById("mobile-nav");
-let sticky = navbar.offsetTop;
-let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-/*let i = 0;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-function hamburger() {
-    if(i == 0) {
-        navbar.classList.add("h-75");
-        i++;
-    } else {
-        navbar.classList.remove("h-75");
-        i--;
-    }
-}*/
+app.use(express.static(path.join(__dirname, 'assets')))
 
-console.log(width);
+app.listen(3000, () => {
+    console.log('Listening port 3000...')
+})
