074cc7bb25ebff0521320516e58faa8833af5c05
BallSpins <sandalupil@gmail.com>
2024-03-24 15:27:54 +0700

    base login page + organized files

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
+    // res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'assets')))
+app.use(express.static(path.join(__dirname, 'app', 'assets')))
 
 app.listen(3000, () => {
     console.log('Listening port 3000...')
